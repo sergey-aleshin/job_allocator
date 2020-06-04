f8c56e1b8f40c40e85c68c140d05ba1e349ec701
Sergey Aleshin <sergey.g.aleshin@gmail.com>
2020-06-04 14:13:08 +0300

    Basic processing of input file and generation of initial state

---
+++ Data.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job_allocator
+{
+    class Agent
+    {
+        public string Name { get; set; }
+        public int MaxUsers { get; set; }
+        public int MaxJobs { get; set; }
+        public List<Job> RunningJobs { get; set; }
+
+        public int AvailableUsers {
+            get
+            {
+                var sum = RunningJobs.Sum(j => j.Users);
+
+                return MaxUsers - sum;
+            }
+        }
+
+        public int AvailableJobs {
+            get
+            {
+                return MaxJobs - RunningJobs.Count;
+            }
+        }
+
+        public bool AddJob(int users, int id, string prefix)
+        {
+            if (users > AvailableUsers)
+                return false;
+
+            if (AvailableJobs < 1)
+                return false;
+
+            RunningJobs.Add(new Job
+            {
+                Id = id,
+                Name = $"{prefix}{id}",
+                Users = users
+            });
+
+            return true;
+        }
+    }
+
+    class Config
+    {
+        public List<Agent> Agents { get; set; }
+    }
+
+    class Job
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Net.Http.Headers;
 
 namespace job_allocator
 {
@@ -11,9 +15,8 @@ namespace job_allocator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(" ", args));
-
             var config = ReadConfig(args);
+            
             PrintConfig(config);
         }
 
@@ -21,53 +24,159 @@ namespace job_allocator
         {
             foreach(var a in config.Agents)
             {
-                Console.WriteLine($"{a.Name, 3} {a.Users, 4} {a.Jobs, 2}");
+                var jobs = string.Join(" ", a.RunningJobs.Select(j => j.Name + '(' + j.Users.ToString() + ')').ToArray());
+                Console.WriteLine($"{a.Name, 3} {a.MaxUsers, 4} {a.MaxJobs, 2} {jobs}");
             }
         }
 
         static Config ReadConfig(string[] args)
         {
             int agents = 20;
-            int users = 1000;
             int jobs = 10;
 
-            return GenerateNewConfig(agents, users, jobs);
+            var readFromFile = false;
+            var customAgentNumber = false;
+            var fileName = string.Empty;
+            int pos = 0;
+
+            while(pos < args.Length)
+            {
+                if (args[pos].ToLower() == "-f")
+                {
+                    pos++;
+
+                    if (pos >= args.Length)
+                        CloseAppWithError("Missing argument for option -f!", -1);
+
+                    fileName = args[pos];
+                    readFromFile = true;
+                } else if(args[pos].ToLower() == "-a")
+                {
+                    pos++;
+
+                    if (pos >= args.Length)
+                        CloseAppWithError("Missing argument for option -a!", -1);
+
+                    if (Regex.Match(args[pos], @"^\d+$").Success)
+                    {
+                        agents = Convert.ToInt32(args[pos]);
+                        customAgentNumber = true;
+                    } else
+                        CloseAppWithError("Argument for option -a should be numeric!", -1);
+                }
+
+                pos++;
+            }
+
+            if (readFromFile && customAgentNumber)
+                CloseAppWithError("You cannot use options -f and -a together!", -1);
+
+            if (!readFromFile)
+                return GenerateNewConfig(agents, jobs);
+
+            return ReadConfigFromFile(fileName);
         }
 
-        static Config GenerateNewConfig(int agents, int users, int jobs)
+        private static void CloseAppWithError(string message, int code)
+        {
+            Console.WriteLine(message);
+            
+            Environment.Exit(code);
+        }
+
+        private static Config ReadConfigFromFile(string fileName)
+        {
+            string[] lines = null;
+
+            try
+            {
+                lines = File.ReadAllLines(fileName);
+            } catch(Exception e)
+            {
+                CloseAppWithError(string.Format("Error: {0}", e.Message), -2);
+            }
+
+            var config = new Config
+            {
+                Agents = new List<Agent>()
+            };
+
+            var counter = 0;
+            var jobCounter = 0;
+
+            foreach(var line in lines)
+            {
+                var match = Regex.Match(line, @"^\s*(\d+):(\d+)\s*(.*)$", RegexOptions.CultureInvariant | RegexOptions.Singleline);
+                if (match.Success)
+                {
+                    var users = match.Groups[1].Value;
+                    var jobs = match.Groups[2].Value;
+                    var rest = match.Groups[3].Value;
+
+                    var runningJobs = rest.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    var a = new Agent
+                    {
+                        MaxUsers = Convert.ToInt32(users),
+                        MaxJobs = Convert.ToInt32(jobs),
+                        Name = $"A{counter}",
+                        RunningJobs = new List<Job>()
+                    };
+
+                    if (runningJobs.Length > 0)
+                    {
+                        foreach(var rj in runningJobs)
+                        {
+                            if (Regex.Match(rj, @"^\d+$").Success && a.AddJob(Convert.ToInt32(rj), jobCounter, "J"))
+                            {
+                                jobCounter++;
+                            }
+                        }
+                    }
+
+                    config.Agents.Add(a);
+                    counter++;
+                }
+            }
+
+            return config;
+        }
+
+        static Config GenerateNewConfig(int agents, int jobs)
         {
             var config = new Config
             {
                 Agents = new List<Agent>()
             };
 
+            var jobCounter = 0;
+            var agentCapacity = new int[] { 10, 20, 50, 100, 200, 500, 1000, 2000, 5000 };
+
             var rand = new Random();
 
             for(var i = 0; i < agents; i++)
             {
+                var capacityIndex = rand.Next(0, agentCapacity.Length);
+
                 var agent = new Agent
                 {
-                    Users = rand.Next(users / 2, users + 1),
-                    Jobs = rand.Next(jobs / 2, jobs + 1),
-                    Name = $"A{i}"
+                    MaxUsers = agentCapacity[capacityIndex],
+                    MaxJobs = rand.Next(jobs / 2, jobs + 1),
+                    Name = $"A{i}",
+                    RunningJobs = new List<Job>()
                 };
+
+                var addJobs = rand.Next(0, 2);
+
+                if(addJobs == 1 && agent.AddJob(rand.Next(1, agent.MaxUsers + 1), jobCounter, "J"))
+                {
+                    jobCounter++;
+                }
 
                 config.Agents.Add(agent);
             }
 
             return config;
         }
-    }
-
-    class Agent
-    {
-        public string Name { get; set; }
-        public int Users { get; set; }
-        public int Jobs { get; set; }
-    }
-
-    class Config
-    {
-        public List<Agent> Agents { get; set; }
     }
 }
